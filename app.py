672bccdaa795358693f9a9388264a1b9003198ee
MarioPEL58 <mpelosin@alice.it>
2026-06-09 19:05:02 +0200

    Add import for render_filters from components.filters

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from components.sidebar import render_sidebar, resolve_file_source
 from components.charts import portfolio_chart
 from components.charts import allocation_pie_chart, allocation_bar_chart
 from components.operations_preview import render_operations_preview
+from components.filters import render_filters
 
 from services.excel_loader import load_dividends_from_excel, load_operations_from_excel
 from services.market_data import download_close_prices, download_last_intraday_timestamp
