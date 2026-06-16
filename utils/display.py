e2f0d6c38a75af609b37ae4a9fa400ca82156615
MarioPEL58 <mpelosin@alice.it>
2026-06-16 16:18:25 +0200

    Add get_display_columns function for column labels

---
+++ utils/display.py
@@ -0,0 +1,24 @@
+from utils.i18n import t
+
+def get_display_columns():
+    return {
+        "Ticker": t("col_ticker"),
+        "Intermediario": t("col_broker"),
+        "Nome": t("col_name"),
+        "Tipo": t("col_type"),
+        "Area": t("col_area"),
+        "Settore": t("col_sector"),
+        "Emittente": t("col_issuer"),
+        "Valuta": t("col_currency"),
+        "Quantita": t("col_quantity"),
+        "Prezzo Attuale": t("col_price_current"),
+        "Valore": t("col_value"),
+        "Dividendi Netti Incassati": t("col_dividends"),
+        "Costo Medio Stimato": t("col_avg_cost"),
+        "Costo Totale Stimato": t("col_total_cost"),
+        "P/L": t("col_pl"),
+        "P/L %": t("col_pl_pct"),
+        "P/L Netto Stimato": t("col_pl_net"),
+        "P/L Giornaliero": t("col_pl_daily"),
+        "P/L Giornaliero %": t("col_pl_daily_pct"),
+    }
