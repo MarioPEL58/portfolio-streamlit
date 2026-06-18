210e3ffdc620fda2a2471d1af89d0b900ce0b77f
MarioPEL58 <mpelosin@alice.it>
2026-06-18 17:22:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from services.excel_loader import load_dividends_from_excel, load_operations_fro
 from services.market_data import download_close_prices, download_last_intraday_timestamp
 from services.portfolio import build_portfolio
 from services.portfolio_metrics import compute_portfolio_xirr, compute_sharpe_ratio
+from services.portfolio_metrics import compute_flow_adjusted_returns, compute_sharpe_from_returns
 from services.market_status import compute_market_update_label
 from services.benchmark import build_flow_adjusted_benchmark
 from utils.formatting import fmt_eur, fmt_pct, style_pl_column
@@ -169,7 +170,14 @@ xirr_value, xirr_flows = compute_portfolio_xirr(
 # =========================
 # sharpe ratio
 # =========================
-sharpe = compute_sharpe_ratio(series["Valore portafoglio"])
+#   not correct   sharpe = compute_sharpe_ratio(series["Valore portafoglio"])
+
+flow_adjusted_returns = compute_flow_adjusted_returns(
+    portfolio_value=series["Valore portafoglio"],
+    flows_df=xirr_flows,
+    flow_col="Operazioni"
+)
+sharpe = compute_sharpe_from_returns(flow_adjusted_returns)
 # =========================
 # Benchmark
 # =========================
