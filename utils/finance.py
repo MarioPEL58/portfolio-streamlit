8c821bda288bc0f24f5020535502c22488927cc9
MarioPEL58 <mpelosin@alice.it>
2026-06-19 14:32:23 +0200

    Add get_dynamic_max function to calculate max value

--- utils/finance.py
+++ utils/finance.py
@@ -10,3 +10,11 @@ def annual_to_daily_rate(r_annual: float, periods_per_year: int = 252) -> float:
         return 0.0
 
     return (1 + r_annual) ** (1 / periods_per_year) - 1
+
+def get_dynamic_max(base, value, benchmark=None):
+    values = [base, value]
+
+    if benchmark is not None and not np.isnan(benchmark):
+        values.append(benchmark)
+
+    return np.ceil(max(values) + 0.5)
