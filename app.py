b39ec71b59b97a4379b5bd37ced4b0820325cc58
MarioPEL58 <mpelosin@alice.it>
2026-07-02 19:14:29 +0200

    Add download closes display to app.py

--- app.py
+++ app.py
@@ -145,6 +145,10 @@ closes, missing = download_close_prices(
 )
 market_last_date = closes.index.max()
 
+st.write("DOWNLOAD CLOSES")
+st.write(closes.tail(10))
+st.write(closes.index)
+
 if closes.empty:
     st.error(t("no_prices"))
     st.stop()
