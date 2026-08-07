9395da5488a9e0924ec4b0b8e966e5ce23b0b58a
MarioPEL58 <mpelosin@alice.it>
2026-08-07 21:00:33 +0200

    Display columns of raw DataFrame

--- services/market_data.py
+++ services/market_data.py
@@ -25,6 +25,7 @@ def download_close_prices(tickers: list[str], start_date: pd.Timestamp, end_date
     # st.write("START", start_date.strftime("%Y-%m-%d"))
     # st.write("END", (end_date + pd.Timedelta(days=1)).strftime("%Y-%m-%d"))
     # st.write(raw)
+    st.write(raw.columns)
 
 
     if raw is None or len(raw) == 0:
