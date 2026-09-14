c5a75a38695abf431dc25741835c38132fbc36d7
MarioPEL58 <mpelosin@alice.it>
2026-09-14 15:23:29 +0200

    Rename variable 'closes' to 'closes_filled'

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.write(
     .head(20)
 )
 
-closes = fill_late_tickers_with_purchase_price(closes,ops,price_col="Prezzo")
+closes_filled = fill_late_tickers_with_purchase_price(closes,ops,price_col="Prezzo")
 st.write("CLOSE DOPO")
 st.write(closes_filled[ticker].loc["2014":"2015"].head(20))
 
