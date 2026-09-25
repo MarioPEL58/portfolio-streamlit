0ae660d668a302785a9df62f28961e9f34a4ea19
MarioPEL58 <mpelosin@alice.it>
2026-09-25 20:02:53 +0200

    Remove unnecessary whitespace in app.py

--- app.py
+++ app.py
@@ -94,16 +94,16 @@ if uploaded_file is not None:
     # Un file caricato manualmente ha priorità sul demo
     st.session_state.use_demo = False
     file_source, file_label = resolve_file_source(uploaded_file)
- 
+    
 elif st.session_state.get("use_demo", False):
     # Mantiene il demo attivo anche dopo i rerun
     file_source = create_demo_file(LANG)
     file_label = "Demo file"
- 
+    
 else:
     file_source = None
     file_label = None
-
+    
 if file_source is None:
     st.info(t("upload_prompt"))
     st.stop()
